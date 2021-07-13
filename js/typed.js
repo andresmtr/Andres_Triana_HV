e496db16a0a94d9ffa03eddae14fd991410f527f
Andres <31396408+andresmtr@users.noreply.github.com>
2021-07-13 12:07:24 -0500

    Ortografia y seleccion de letra

--- js/typed.js
+++ js/typed.js
@@ -1,8 +1,8 @@
 //  animation escritura
 var typed = new Typed('.typed', {
-    strings: ['Ingeniero de sistemas', 'Analisis de datos', 'Desarrollo web - Front End', 'Monitoreo y seguimiento'],
-    typeSpeed: 20,
-    backSpeed: 20,
+    strings: ['Ingeniero de sistemas', 'Análisis de datos', 'Desarrollo web - Front End', 'Monitoreo y seguimiento'],
+    typeSpeed: 10,
+    backSpeed: 10,
     fadeOut: true,
     loop: true,
     // showCursor: false
