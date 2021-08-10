fbdb529f7d79664723c24c1b403ac9df99cf0786
Andres <31396408+andresmtr@users.noreply.github.com>
2021-08-10 09:19:29 -0500

    Actualización habilidades

--- js/typed.js
+++ js/typed.js
@@ -1,6 +1,6 @@
 //  animation escritura
 var typed = new Typed('.typed', {
-    strings: ['Ingeniero de sistemas', 'Análisis de datos', 'Desarrollo web - Front End', 'Monitoreo y seguimiento'],
+    strings: ['Ingeniero de sistemas', 'Data analytics and visualization', 'Full stack developer', 'Monitoreo y seguimiento'],
     typeSpeed: 10,
     backSpeed: 10,
     fadeOut: true,
